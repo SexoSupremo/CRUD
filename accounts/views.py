f70e7af5e65b09d9a030cba9a44a35c0c3291615
Esteban <ehequiel911@gmail.com>
2024-11-16 03:10:42 -0300

    d

--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,17 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth import login, authenticate
+from .forms import CustomUserCreationForm  # Añadido
 
 class UserCreateAndLoginView(CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     template_name = "accounts/signup.html"
     success_url = reverse_lazy("blog:index")
 
     def form_valid(self, form):
         response = super().form_valid(form)
-        username = form.cleaned_data.get("username")
+        email = form.cleaned_data.get("email")
         raw_pw = form.cleaned_data.get("password1")
-        user = authenticate(username=username, password=raw_pw)
-        if user is not None:
-            login(self.request, user)
+        user = authenticate(email=email, password=raw_pw)
+        login(self.request, user)
         return response
