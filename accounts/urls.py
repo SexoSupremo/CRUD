9011fffc6dd53274a7deda2a9fc9f4e3faa5a0e4
Esteban <ehequiel911@gmail.com>
2024-11-16 02:47:00 -0300

    d

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import CreateView  # Añadido
+from django.contrib.auth.forms import UserCreationForm  # Añadido
 
 urlpatterns = [
+    path('signup/', CreateView.as_view(
+        template_name='accounts/signup.html',
+        form_class=UserCreationForm,
+        success_url='/accounts/login',
+    ), name='signup'),
+    # Añadido
     path('login/', LoginView.as_view(
         redirect_authenticated_user=True,
         template_name='accounts/login.html'
