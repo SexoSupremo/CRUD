e8eebe136384da844af1ff81f48daa02358d02ce
Esteban <ehequiel911@gmail.com>
2024-11-16 02:36:06 -0300

    d

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from django.contrib import admin
 app_name = 'blog'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
-    path('accounts/', include('accounts.urls')), 
+    path('', views.index, name='index'),
+    path('create', views.create, name='create'),
+    path('detail/<int:article_id>', views.detail, name='detail'),
+    path('edit/<int:article_id>', views.edit, name='edit'),
+    path('delete/<int:article_id>', views.delete, name='delete'),
 ]
